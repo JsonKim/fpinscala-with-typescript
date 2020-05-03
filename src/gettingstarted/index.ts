739415e8bbb3ea2928c70a76ea691082a411a2bf
Jason Kim <actcrazy@gmail.com>
2020-05-03 18:42:09 +0900

    연습문제 2.2

--- src/gettingstarted/index.ts
+++ src/gettingstarted/index.ts
@@ -17,6 +17,10 @@ module MyModule {
     console.log(formatFactorial(7))
     console.log(formatResult('absolute value', -42, abs))
     console.log(formatResult('factorial', 7, factorial))
+    console.log(isSorted([1, 2, 3, 4], (x, y) => x < y))
+    console.log(isSorted([1, 2, 3, 4], (x, y) => x > y))
+    console.log(isSorted([1, 2, 2, 4], (x, y) => x < y))
+    console.log(isSorted([1], (x, y) => x > y))
   }
 
   export const factorial = (n: number): number => {
@@ -40,6 +44,16 @@ module MyModule {
   const formatFactorial = (n: number): string => `The factorial of ${n} is ${factorial(n)}`
 
   const formatResult = (name: string, n: number, f: (x: number) => number) => `The ${name} of ${n} is ${f(n)}`
+
+  const isSorted = <A>(as: Array<A>, ordered: (x: A, y: A) => boolean): boolean => {
+    const go = (n: number): boolean => {
+      if (n >= as.length - 1) return true
+      else if (ordered(as[n + 1], as[n])) return false
+      else return go(n + 1)
+    }
+
+    return go(0)
+  }
 }
 
 MyModule.main()
