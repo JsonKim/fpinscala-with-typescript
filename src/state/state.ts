1c3538e16bb6a09e78d8073ff0baf40443f95499
Jason Kim <actcrazy@gmail.com>
2020-05-09 14:06:42 +0900

    연습문제 6.1~6.4

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,7 +1,13 @@
+import { List, nil, cons } from '../datastructures/list'
+
+const INT_MAX_VALUE = 2 ** 31 - 1
 
 namespace RNG {
+  type Int = number
+  type Double = number
+
   abstract class RNG {
-    abstract nextInt(): [number, RNG]
+    abstract nextInt(): [Int, RNG]
   }
 
   class SimpleRNG extends RNG {
@@ -9,7 +15,7 @@ namespace RNG {
       super()
     }
 
-    nextInt(): [number, RNG] {
+    nextInt(): [Int, RNG] {
       // eslint-disable-next-line no-bitwise
       const newSeed = (this.seed * 0x5DEECE66Dn + 0xBn) & 0xFFFFFFFFFFFFn
       const nextRNG = new SimpleRNG(newSeed)
@@ -32,6 +38,53 @@ namespace RNG {
       return [f(a), rng2]
     }
 
+  const nonNegativeInt = (rng: RNG): [Int, RNG] =>
+    map(int)(x => (x < 0 ? -(x + 1) : x))(rng)
+
+  const double = (rng: RNG): [Double, RNG] =>
+    map(nonNegativeInt)(n => n / (INT_MAX_VALUE + 1))(rng)
+
+  const intDouble = (rng: RNG): [[Int, Double], RNG] => {
+    const [n, rng2] = rng.nextInt()
+    const [d, rng3] = double(rng2)
+    return [[n, d], rng3]
+  }
+
+  const doubleInt = (rng: RNG): [[Double, Int], RNG] => {
+    const [d, rng2] = double(rng)
+    const [n, rng3] = rng2.nextInt()
+    return [[d, n], rng3]
+  }
+
+  const doubleInt2 = (rng: RNG): [[Double, Int], RNG] =>
+    map(doubleInt)(([d, n]): [Int, Double] => [n, d])(rng)
+
+  const double3 = (rng: RNG): [[Double, Double, Double], RNG] => {
+    const [d1, rng2] = double(rng)
+    const [d2, rng3] = double(rng2)
+    const [d3, rng4] = double(rng3)
+    return [[d1, d2, d3], rng4]
+  }
+
+  const ints = (count: Int) => (rng: RNG): [List<Int>, RNG] => {
+    if (count <= 0) return [nil, rng]
+
+    const [n, rng2] = rng.nextInt()
+    const [l, rng3] = ints(count - 1)(rng2)
+    return [cons(n, l), rng3]
+  }
+
+  const ints2 = (count: Int) => (rng: RNG): [List<Int>, RNG] => {
+    const go = (count: Int, rng: RNG, l: List<Int>): [List<Int>, RNG] => {
+      if (count <= 0) return [l, rng]
+
+      const [n, rng2] = int(rng)
+      return go(count - 1, rng2, cons(n, l))
+    }
+
+    return go(count, rng, nil)
+  }
+
   export const main = () => {
     const rng = new SimpleRNG(42n)
 
@@ -40,6 +93,11 @@ namespace RNG {
 
     const [n2, rng3] = rng2.nextInt()
     console.log(n2)
+
+    const nn1 = nonNegativeInt(rng2)
+    console.log(nn1)
+
+    console.log(ints(3)(rng))
   }
 }
 
