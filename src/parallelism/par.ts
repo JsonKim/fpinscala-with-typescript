7a80ba3fd6a6285ee68b4bf8b0b6a960a8056578
Jason Kim <actcrazy@gmail.com>
2020-05-10 13:29:51 +0900

    연습문제 7.1

--- src/parallelism/par.ts
+++ src/parallelism/par.ts
@@ -9,6 +9,9 @@ namespace Par {
   export const unit = <A>(a: Lazy<A>): Par<A> => () => a()
 
   export const get = <A>(a: Par<A>): A => a()
+
+  export const map2 = <A>(p1: Par<A>, p2: Par<A>) => <B>(f: (a1: A, a2: A) => B): Par<B> =>
+    unit(() => f(get(p1), get(p2)))
 }
 
 namespace example {
@@ -27,8 +30,16 @@ namespace example {
     if (ints.length === 1) return ints[0]
 
     const [l, r] = R.splitAt(ints.length / 2, ints)
-    const sumL = Par.unit(() => sum(l))
-    const sumR = Par.unit(() => sum(r))
+    const sumL = Par.unit(() => sum2(l))
+    const sumR = Par.unit(() => sum2(r))
     return Par.get(sumL) + Par.get(sumR)
+  }
+
+  const sum3 = (ints: Array<number>): Par<number> => {
+    if (ints.length === 0) return Par.unit(() => 0)
+    if (ints.length === 1) return Par.unit(() => ints[0])
+
+    const [l, r] = R.splitAt(ints.length / 2, ints)
+    return Par.map2(sum3(l), sum3(r))((l, r) => l + r)
   }
 }
